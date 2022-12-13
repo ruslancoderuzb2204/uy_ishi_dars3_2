3bf17e1a18151237a66d65a734b9e68a2b4aa2e5
ruslantilovov2 <ruslantilovov2@gmail.com>
2022-12-13 09:40:53 +0500

    full

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Faqat musbat son kiriting!")
+let a = +prompt("Son kiriting!");
+let b = +prompt("Son kiriting!");
+let c = +prompt("Son kiriting!");
+let d = a + b + c;
+if(Math.sqrt(d) % 2 == 0 || Math.sqrt(d) % 2 == 1  ){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
